30e515c0d8c34042c26eff6459b845d82ed3447c
matthieu.laniesse@gmail.com <bevada09>
2017-10-09 10:43:27 +0200

    Test sur la premiere fonction (vert)

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,22 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+    var colonne = c;
+    var ligne = l;
+
+    var goodCoordinates = ['A3'
+        ,'B2','B3','B4','B5'
+        ,'C1','C2','C3','C4','C5','C6','C7'
+        ,'D2','D3','D4','D5','D6','D7'
+        ,'E2','E3','E4','E5','E6','E7','E8'
+        ,'F3','F4','F5','F6','F7','F8'
+        ,'G3','G4','G5','G6','G7','G8','G9'
+        ,'H5','H6','H7','H8'
+        ,'I7'];
+
+
+
+    this.isValid = function() {
+        return goodCoordinates.indexOf(colonne + ligne) >= 0;
+    }
 };
