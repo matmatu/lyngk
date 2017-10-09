b9e5b79573da69745c3f6d8a988752fda2b40e13
matthieu.laniesse@gmail.com <bevada09>
2017-10-09 11:00:53 +0200

    Premier test 2.2 (43)

--- test/test.js
+++ test/test.js
@@ -5,4 +5,23 @@ LyngkTestCase.prototype.testCoordinate = function()
 {
     var coordone = new Lyngk.Coordinates('A',1);
     assertFalse(coordone.isValid());
+
+
+};
+LyngkTestCase.prototype.test43 = function()
+{
+    var compteur = 0;
+    var lettre = ("ABCDEFGHI");
+
+    for(var i = 0; i < lettre.length;i++)
+    {
+        for(var j = 1; j <= 9 ; j++){
+            var tempo = new Lyngk.Coordinates(lettre[i],j);
+            if(tempo.isValid()){
+                compteur++;
+            }
+        }
+    }
+    assertEquals(count,43);
+
 };
