dc0b1022e2cc55be5732bb20cb54b1e2f58a08ee
matthieu.laniesse@gmail.com <bevada09>
2017-10-09 10:41:37 +0200

    premier test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+LyngkTestCase.prototype.testCoordinate = function()
+{
+    var coordone = new Lyngk.Coordinates('A',1);
+    assertFalse(coordone.isValid());
+};
