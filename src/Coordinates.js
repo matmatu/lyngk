85fc54ba276cc72435a51fe9edb49e65868a9a2d
matthieu.laniesse@gmail.com <bevada09>
2017-10-09 11:25:33 +0200

    test fonction 2.3 (tostring)

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,10 +1,9 @@
 "use strict";
-
-Lyngk.Coordinates = function (c, l) {
+Lyngk.Coordinates = function (c, l)
+{
     var colonne = c;
     var ligne = l;
-
-    var goodCoordinates = ['A3'
+    var tableau = ['A3'
         ,'B2','B3','B4','B5'
         ,'C1','C2','C3','C4','C5','C6','C7'
         ,'D2','D3','D4','D5','D6','D7'
@@ -14,9 +13,15 @@ Lyngk.Coordinates = function (c, l) {
         ,'H5','H6','H7','H8'
         ,'I7'];
 
-
-
-    this.isValid = function() {
-        return goodCoordinates.indexOf(colonne + ligne) >= 0;
-    }
+    this.isValid = function()
+    {
+        return tableau.indexOf(colonne+ligne) >= 0;
+    };
+    this.toString = function()
+    {
+        if(this.isValid())
+            return colonne+ligne;
+        else
+            return "non defini";
+    };
 };
