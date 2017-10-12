de1eb9b12ccf53e0bd5879b4f229e475986d1c9e
matthieu <matthieu.laniesse@gmail.com>
2017-10-12 22:22:26 +0200

    2.8 fonction

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,4 +3,31 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
+    var state = Lyngk.State.VACANT;
+    var pieces = [];
+
+    this.getState = function(){
+        return state;
+    };
+
+    this.getPieces = function(){
+        return pieces;
+    };
+
+    this.setPiece = function(p){
+        pieces.push(p);
+        if (pieces.length == 1)
+            state = Lyngk.State.ONE_PIECE;
+        else if (pieces.length >= 5)
+            state = Lyngk.State.FULL_STACK;
+        else
+            state = Lyngk.State.STACK;
+    };
+
+    this.getColor = function(){
+        if (pieces.length > 0)
+            return pieces[pieces.length-1].getColor();
+        else
+            return null;
+    };
 };
